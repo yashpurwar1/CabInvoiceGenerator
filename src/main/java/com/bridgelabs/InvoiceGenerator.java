ffa34f17319b611da28364b569f81601f67e870f
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-08-10 17:12:57 +0530

    Added: method to add premium rides for all test cases

--- src/main/java/com/bridgelabs/InvoiceGenerator.java
+++ src/main/java/com/bridgelabs/InvoiceGenerator.java
@@ -2,14 +2,11 @@ package com.bridgelabs;
 
 public class InvoiceGenerator {
 
-    /**
-     * Given : MINIMUM_COST_PER_KM = 10.0
-     *         COST_PER_TIME = 1
-     *         MINIMUM_FARE = 5
-     */
-    private static final double MINIMUM_COST_PER_KM = 10.0;
-    private static final int COST_PER_TIME = 1;
-    private static final double MINIMUM_FARE = 5;
+    private static final int NORMALRIDE = 1;
+    private static final int PREMIUMRIDE = 2;
+    private static double MINIMUM_COST_PER_KM = 0.0;
+    private static int COST_PER_TIME = 0;
+    private static double MINIMUM_FARE = 0;
 
     /**
      * Purpose : Given distance and time,
@@ -20,9 +17,28 @@ public class InvoiceGenerator {
      * @param distance
      * @param time
      */
-    public double calculateFare(double distance, int time) {
-        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+    public double calculateFare(double distance, int time, int option) {
 
+        /**
+         * Given Values For Normal Ride
+         */
+
+        if(option == NORMALRIDE) {
+            MINIMUM_COST_PER_KM = 10.0;
+            COST_PER_TIME = 1;
+            MINIMUM_FARE = 5;
+        }
+
+        /**
+         * Given Values For Premium Ride
+         */
+        else if(option == PREMIUMRIDE) {
+            MINIMUM_COST_PER_KM = 15.0;
+            COST_PER_TIME = 2;
+            MINIMUM_FARE = 20;
+        }
+
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
         return Math.max(totalFare, MINIMUM_FARE);
     }
 
@@ -32,18 +48,25 @@ public class InvoiceGenerator {
      *
      * @param rides
      */
-    public double calculateTotalFare(Ride[] rides) {
+    public double calculateTotalFare(Ride[] rides, int option) {
         double totalFare = 0.0;
         for(Ride ride : rides) {
-            totalFare += this.calculateFare(ride.distance, ride.time);
+            totalFare += this.calculateFare(ride.distance, ride.time, option);
         }
         return totalFare;
     }
 
-    public InvoiceSummary calculateFareSummary(Ride[] rides) {
+    /**
+     * Purpose : Given distance and time for multiple rides,
+     *           Calculate aggregate total fare for all the journey
+     *           Calculate the average fare per ride taking total number of rides and total fare as input
+     *
+     * @param rides
+     */
+    public InvoiceSummary calculateFareSummary(Ride[] rides, int option) {
         double totalFare = 0.0;
         for(Ride ride : rides) {
-            totalFare += this.calculateFare(ride.distance, ride.time);
+            totalFare += this.calculateFare(ride.distance, ride.time, option);
         }
         return new InvoiceSummary(rides.length, totalFare);
     }
--- src/test/java/com/bridgelabs/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabs/InvoiceServiceTest.java
@@ -1,6 +1,5 @@
 package com.bridgelabs;
 
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +7,8 @@ import org.junit.Test;
 import java.util.Hashtable;
 
 public class InvoiceServiceTest {
+    private static final int NORMALRIDE = 1;
+    private static final int PREMIUMRIDE = 2;
     InvoiceGenerator invoiceGenerator = null;
 
     /**
@@ -28,21 +29,34 @@ public class InvoiceServiceTest {
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance,time);
-        Assert.assertEquals(25, fare, 0.0);
+        int option = NORMALRIDE;
+
+        double fare = invoiceGenerator.calculateFare(distance, time, option);
+
+        if(option == NORMALRIDE)
+            Assert.assertEquals(25, fare, 0.0);
+        else if(option == PREMIUMRIDE)
+            Assert.assertEquals(40, fare, 0.0);
     }
 
     /**
      * Step 1.2
      * Purpose: Given distance and time,
-     *          return minimum fare as 5
+     *          return minimum fare as MINIMUM_FARE
      */
+
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
         double distance = 0.1;
         int time = 1;
-        double fare = invoiceGenerator.calculateFare(distance,time);
-        Assert.assertEquals(5, fare, 0.0);
+        int option = PREMIUMRIDE;
+
+        double fare = invoiceGenerator.calculateFare(distance, time, option);
+
+        if(option == NORMALRIDE)
+            Assert.assertEquals(5, fare, 0.0);
+        else if(option == PREMIUMRIDE)
+            Assert.assertEquals(20, fare, 0.0);
     }
 
     /**
@@ -52,25 +66,42 @@ public class InvoiceServiceTest {
      */
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare() {
+        int option = PREMIUMRIDE;
+
         Ride[] rides = { new Ride(2.0, 5),
                 new Ride(0.1, 1) };
-        double fare = invoiceGenerator.calculateTotalFare(rides);
-        Assert.assertEquals(30, fare, 0.0);
+
+        double fare = invoiceGenerator.calculateTotalFare(rides, option);
+
+        if(option == NORMALRIDE)
+            Assert.assertEquals(30, fare, 0.0);
+        else if(option == PREMIUMRIDE)
+            Assert.assertEquals(60, fare, 0.0);
     }
 
     /**
      * Step 3
      * Purpose : Given distance and time,
      *           calculate the aggregate total for all the rides
-     *           finally calculate the average fare per ride.
+     *           finally calculate the average fare per ride
      */
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        int option = PREMIUMRIDE;
+
         Ride[] rides = { new Ride(2.0, 5),
                 new Ride(0.1, 1) };
-        InvoiceSummary summary = invoiceGenerator.calculateFareSummary(rides);
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
-        Assert.assertEquals(expectedInvoiceSummary, summary);
+
+        InvoiceSummary summary = invoiceGenerator.calculateFareSummary(rides, option);
+
+        if(option == NORMALRIDE) {
+            InvoiceSummary expectdInvoiceSummary = new InvoiceSummary(2, 30);
+            Assert.assertEquals(expectdInvoiceSummary, summary);
+        }
+        else if(option == PREMIUMRIDE) {
+            InvoiceSummary expectdInvoiceSummary = new InvoiceSummary(2, 60);
+            Assert.assertEquals(expectdInvoiceSummary, summary);
+        }
     }
 
     /**
@@ -83,6 +114,8 @@ public class InvoiceServiceTest {
     @Test
     public void givenUserID_ShouldReturnInvoiceSummary() {
         Hashtable<Integer, Ride[]> htable = new Hashtable<>();
+
+        int option = PREMIUMRIDE;
 
         int userID1 = 1;
         Ride[] ride1 = { new Ride(2.0, 5),
@@ -97,9 +130,15 @@ public class InvoiceServiceTest {
         int userID = 2;
 
         if(htable.containsKey(userID)) {
-            InvoiceSummary summary = invoiceGenerator.calculateFareSummary(htable.get(userID));
-            InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 61);
-            Assert.assertEquals(expectedInvoiceSummary, summary);
+            InvoiceSummary summary = invoiceGenerator.calculateFareSummary(htable.get(userID), option);
+            if(option == NORMALRIDE) {
+                InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 61);
+                Assert.assertEquals(expectedInvoiceSummary, summary);
+            }
+            else if(option == PREMIUMRIDE) {
+                InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 100);
+                Assert.assertEquals(expectedInvoiceSummary, summary);
+            }
         }
     }
 }
